4b7b0f2a7a3a2ad1abdee01f048b8ff8ca47f32b
Milizee <gridiffin6603@gmail.com>
2025-01-09 09:45:24 +0800

    add flutter_dotenv package and update .gitignore

--- lib/src/screens/chat/chatscreen.dart
+++ lib/src/screens/chat/chatscreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
